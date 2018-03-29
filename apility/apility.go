1e98688522ec4cb2b946f60abeebcb94b7e00ba4
nihalpasham <nihal.pasham@gmail.com>
2018-03-29 21:28:00 +0530

    tidying up a few things i.e. format

--- apility/apility.go
+++ apility/apility.go
@@ -1,50 +1,47 @@
 package apility
 
 import (
-		"fmt"
-		"flag"
-		"net/http"
-		"io"
-		"encoding/json"
-		"net/http/httputil"
-		"os"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httputil"
+	"os"
 )
 
 const (
-	   baseURL  =  "https://api.apility.net/"
-	   apiKey	 =  ""								//get an API key from apility.io by signing up
+	baseURL = "https://api.apility.net/"
+	apiKey  = "" //get an API key from apility.io by signing up
 )
 
-var (	
-	 domain   string
-	 ip		  string
-	 url      string
+var (
+	domain string
+	ip     string
+	url    string
 )
 
 func init() {
 
 	flag.StringVar(&domain, "d", "", "domain to be looked up")
-	flag.StringVar(&ip, "ip", "", "IP to be looked up")				
+	flag.StringVar(&ip, "ip", "", "IP to be looked up")
 }
 
-func newClient() (*Client) {
+func newClient() *Client {
 
-	c := &Client {
+	c := &Client{
 		entryPoint: baseURL,
-		c: 	 		http.DefaultClient,
-		apiKey:		apiKey,
-		}
+		c:          http.DefaultClient,
+		apiKey:     apiKey,
+	}
 	return c
 }
 
-
 // Client interacts with the services provided by apility.io
 type Client struct {
-
-	c 			*http.Client 
-	entryPoint 	string
-	apiKey 		string
-
+	c          *http.Client
+	entryPoint string
+	apiKey     string
 }
 
 func (c *Client) domainsearch(domain string) (*DomainSearch, error) {
@@ -68,77 +65,74 @@ func (c *Client) ipsearch(ip string) (*ipSearch, error) {
 }
 
 func (c *Client) query(method, url string, body io.Reader, result interface{}) error {
-			
-			req, err := http.NewRequest(method, url, body)
-			if err != nil {
-				return err
-			} 
-			req.Header.Set("Accept", "application/json")
-			req.Header.Set("X-Auth-Token", c.apiKey)
 
-			resp, err := c.c.Do(req)
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("X-Auth-Token", c.apiKey)
 
-			if err != nil {
-				return err
+	resp, err := c.c.Do(req)
+
+	if err != nil {
+		return err
+	}
+	fmt.Printf("http StatusCode : %#v\n\n", resp.StatusCode)
+
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	switch resp.StatusCode {
+	case 200:
+		err = json.NewDecoder(resp.Body).Decode(result)
+		if err != nil {
+			out, err := httputil.DumpResponse(resp, true)
+			if err == nil {
+				fmt.Printf("%s\n", string(out))
 			}
-			fmt.Printf("http StatusCode : %#v\n\n", resp.StatusCode)
-
-			if resp.Body != nil {
-				defer resp.Body.Close()
-			}
-
-			switch resp.StatusCode {	
-				case 200:
-					err = json.NewDecoder(resp.Body).Decode(result)
-					if err != nil {
-						out, err := httputil.DumpResponse(resp, true)
-						if err == nil {
-							fmt.Printf("%s\n", string(out))
-						}
-					}
-				case 404:
-					fmt.Printf("%v is NOT in any of the blacklists\n", domain)
-				default:
-					fmt.Printf("Something went wrong! Recheck the domain/ip ...\n\n",)
-					fmt.Fprintf(os.Stderr, "exiting with StatusCode : %#v ...\n", resp.StatusCode)
-					os.Exit(1)
-				}
-			return nil
 		}
+	case 404:
+		fmt.Printf("%v is NOT in any of the blacklists\n", domain)
+	default:
+		fmt.Printf("Something went wrong! Recheck the domain/ip ...\n\n")
+		fmt.Fprintf(os.Stderr, "exiting with StatusCode : %#v ...\n", resp.StatusCode)
+		os.Exit(1)
+	}
+	return nil
+}
 
-
-// DomainSearch holds the full bad domain details if any 
+// DomainSearch holds the full bad domain details if any
 type DomainSearch struct {
-
-	Type    string    `json:"type"`
-	Response   struct {  	    
-		Domain   struct {     
-			MX    	  	  []string	  `json:"mx"`
-			NS            []string    `json:"ns"`
-			BlacklistMx   []string    `json:"blacklist_mx"`
-			BlacklistNs   []string    `json:"blacklist_ns"`
-			Blacklist	  []string	  `json:"blacklist"`
-			score		  int		  `json:"score"`
-		}`json:"domain"`
-		Score 	   int		`json:"score"`
-		SourceIP  struct {
-			Score		   int		`json:"score"`
-			Address	  	   string	`json:"address"`		   	
-			IsQuarantined  bool		`json:"is_quarantined"`
-			Blacklist      []string	`json:"blacklist"`
+	Type     string `json:"type"`
+	Response struct {
+		Domain struct {
+			MX          []string `json:"mx"`
+			NS          []string `json:"ns"`
+			BlacklistMx []string `json:"blacklist_mx"`
+			BlacklistNs []string `json:"blacklist_ns"`
+			Blacklist   []string `json:"blacklist"`
+			score       int      `json:"score"`
+		} `json:"domain"`
+		Score    int `json:"score"`
+		SourceIP struct {
+			Score         int      `json:"score"`
+			Address       string   `json:"address"`
+			IsQuarantined bool     `json:"is_quarantined"`
+			Blacklist     []string `json:"blacklist"`
 		} `json:"source_ip"`
-		IP   struct {
-			Score   	   int 		`json:"score"`
-			Address		   string	`json:"address`
-			IsQuarantined  bool		`json:"is_quarantined"`
-			Blacklist	   []string	`json:"blacklist"`
-		}`json:"ip"`
-					
-	}`json:"response"`
+		IP struct {
+			Score         int      `json:"score"`
+			Address       string   `json:"address`
+			IsQuarantined bool     `json:"is_quarantined"`
+			Blacklist     []string `json:"blacklist"`
+		} `json:"ip"`
+	} `json:"response"`
 }
 
 // ipSearch holds the list of all blacklist for a given IP (if any)
 type ipSearch struct {
-	Type   		string		`json:"type"`
-	Response	[]string	`json:"response"`
+	Type     string   `json:"type"`
+	Response []string `json:"response"`
 }
--- apility/main.apility.go
+++ apility/main.apility.go
@@ -1,28 +1,22 @@
 package main
 
 import (
-		"fmt"
-		"flag"
-		"net/http"
-		"io"
-		"encoding/json"
-		"net/http/httputil"
-		"os"
-		"github.com/nihalpasham/secautomation/apility"
+	"encoding/json"
+	"flag"
+	"fmt"
 )
 
-var (	
-	 domain   string
-	 ip		 string
-	 url      string
+var (
+	domain string
+	ip     string
+	url    string
 )
 
 func init() {
 
 	flag.StringVar(&domain, "d", "", "domain to be looked up")
-	flag.StringVar(&ip, "ip", "", "IP to be looked up")				
+	flag.StringVar(&ip, "ip", "", "IP to be looked up")
 }
-
 
 func main() {
 
@@ -30,28 +24,28 @@ func main() {
 	c := newClient()
 
 	switch {
-		case domain != "":
-			fmt.Println("checking domain:", domain)
-			res, err := c.domainsearch(domain)
-			if err != nil {
-				fmt.Printf("Error : %#v\n", err)
-				} else {
-					b, err := json.MarshalIndent(res, "", "\t")
-					if err == nil {
-					fmt.Println(string(b))
-					}	
-				}
-		case ip != "":
-			fmt.Println("checking ip:", ip)
-			res, err := c.ipsearch(ip)
-			if err != nil {
-				fmt.Printf("Error : %#v\n", err)
-			} else {
-				b, err := json.MarshalIndent(res, "", "\t")
-				if err == nil {
-					fmt.Println(string(b))
-				}
+	case domain != "":
+		fmt.Println("checking domain:", domain)
+		res, err := c.domainsearch(domain)
+		if err != nil {
+			fmt.Printf("Error : %#v\n", err)
+		} else {
+			b, err := json.MarshalIndent(res, "", "\t")
+			if err == nil {
+				fmt.Println(string(b))
 			}
 		}
+	case ip != "":
+		fmt.Println("checking ip:", ip)
+		res, err := c.ipsearch(ip)
+		if err != nil {
+			fmt.Printf("Error : %#v\n", err)
+		} else {
+			b, err := json.MarshalIndent(res, "", "\t")
+			if err == nil {
+				fmt.Println(string(b))
+			}
+		}
+	}
 
 }
